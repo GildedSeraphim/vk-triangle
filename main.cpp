4d5490bf3aeaae7bf3c386326f2b5487c3f50833
GildedSeraphim <snathani@protonmail.com>
2025-01-05 19:32:07 -0600

    vase

--- main.cpp
+++ main.cpp
@@ -31,9 +31,8 @@
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
-const std::string MODEL_PATH = "models/viking_room.obj";
-const std::string TEXTURE_PATH = "textures/viking_room.png";
-
+const std::string MODEL_PATH = "./models/WB Vase.obj";
+const std::string TEXTURE_PATH = "./textures/WB_Vase_Mat_Base_color.jpg";
 const int MAX_FRAMES_IN_FLIGHT = 2;
 
 const std::vector<const char *> validationLayers = {
